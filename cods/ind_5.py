7df51073939dd75c978ba34b4490f6bbd948ae32
StepanPS <145998536+StepanPS@users.noreply.github.com>
2023-10-27 23:08:25 +0500

    Create ind_5.py

---
+++ cods/ind_5.py
@@ -0,0 +1,7 @@
+from Ind_5_1 import Geron
+print("Введите стороны треугольника:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+S = Geron(a, b, c)
+print(f"Площадь треугольника = {S}")
