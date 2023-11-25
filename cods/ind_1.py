7ae37e4754a75ab1160e84d6f3d7c096521cb1bd
StepanPS <145998536+StepanPS@users.noreply.github.com>
2023-11-25 18:57:10 +0500

    Create ind_1.py

---
+++ cods/ind_1.py
@@ -0,0 +1,9 @@
+class Literature:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+book = Literature("War of the Worlds", "Herbert Wells")
+print(book.title)
+print(book.author)
