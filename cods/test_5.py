bd2c0b43627a2d4f5a6214dd0562bf13839e9326
StepanPS <145998536+StepanPS@users.noreply.github.com>
2023-12-01 19:15:42 +0500

    Create test_5.py

---
+++ cods/test_5.py
@@ -0,0 +1,8 @@
+from ind.classes import TomatoBush, Gardener
+
+if __name__ == '__main__':
+    bush = TomatoBush(10)
+    gardener = Gardener("Пётр", bush)
+    for _ in range(3):
+        gardener.work()
+    gardener.harvest()
